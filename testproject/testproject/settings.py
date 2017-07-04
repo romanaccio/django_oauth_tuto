5efb959954f18e197db5e3849ae36ec84d3b2eb0
Antoine Jouannais <antoine.jouannais@gmail.com>
2017-07-04 16:49:00 +0200

    tuto 2

--- testproject/testproject/settings.py
+++ testproject/testproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'oauth2_provider',
     'corsheaders',
+	'myapp',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'testproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# ne pas laisser CORS_ORIGIN_ALLOW_ALL à True en production
+CORS_ORIGIN_ALLOW_ALL = True
--- testproject/testproject/urls.py
+++ testproject/testproject/urls.py
@@ -13,10 +13,44 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
+
 from django.conf.urls import url, include
+import oauth2_provider.views as oauth2_views
+from django.conf import settings
+from myapp.views import ApiEndpoint
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
+# OAuth2 provider endpoints
+oauth2_endpoint_views = [
+    url(r'^authorize/$', oauth2_views.AuthorizationView.as_view(), name="authorize"),
+    url(r'^token/$', oauth2_views.TokenView.as_view(), name="token"),
+    url(r'^revoke-token/$', oauth2_views.RevokeTokenView.as_view(), name="revoke-token"),
+]
+
+if settings.DEBUG:
+    # OAuth2 Application Management endpoints
+    oauth2_endpoint_views += [
+        url(r'^applications/$', oauth2_views.ApplicationList.as_view(), name="list"),
+        url(r'^applications/register/$', oauth2_views.ApplicationRegistration.as_view(), name="register"),
+        url(r'^applications/(?P<pk>\d+)/$', oauth2_views.ApplicationDetail.as_view(), name="detail"),
+        url(r'^applications/(?P<pk>\d+)/delete/$', oauth2_views.ApplicationDelete.as_view(), name="delete"),
+        url(r'^applications/(?P<pk>\d+)/update/$', oauth2_views.ApplicationUpdate.as_view(), name="update"),
+    ]
+
+    # OAuth2 Token Management endpoints
+    oauth2_endpoint_views += [
+        url(r'^authorized-tokens/$', oauth2_views.AuthorizedTokensListView.as_view(), name="authorized-token-list"),
+        url(r'^authorized-tokens/(?P<pk>\d+)/delete/$', oauth2_views.AuthorizedTokenDeleteView.as_view(),
+            name="authorized-token-delete"),
+    ]
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    # OAuth 2 endpoints:
+    url(r'^o/', include(oauth2_endpoint_views, namespace="oauth2_provider")),
+    url(r'^myapp/hello', ApiEndpoint.as_view()),  # an example resource endpoint
+	url(r'^accounts/login/$', auth_views.LoginView.as_view(), name='login'),
+
 ]
